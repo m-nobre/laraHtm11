3fff3e95878d35683e4ca6b1e4ad423a7c99ec69
Mário Nobre <m.nobre@ymail.com>
2024-12-17 18:50:39 +0000

    vite public

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,11 @@ export default defineConfig({
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
+            publicDirectory: 'public_html',
         }),
     ],
+    build: {
+        outDir: "public_html/build",
+      },
+      
 });
